20b10889d603d61977c2e30f08c8b9ecac5a3195
Andrey Korzh <ao.korzh@gmail.com>
2026-05-28 15:59:46 +0300

    fix old repo compatibility issues with modern colab

--- saicinpainting/training/data/aug.py
+++ saicinpainting/training/data/aug.py
@@ -1,19 +1,18 @@
-from albumentations import DualIAATransform, to_tuple
+from albumentations.core.transforms_interface import DualTransform, to_tuple
 import imgaug.augmenters as iaa
+import numpy as np
 
-class IAAAffine2(DualIAATransform):
-    """Place a regular grid of points on the input and randomly move the neighbourhood of these point around
-    via affine transformations.
 
-    Note: This class introduce interpolation artifacts to mask if it has values other than {0;1}
+def _apply_iaa(img, augmenter):
+    if img.ndim == 2:
+        img = img[:, :, np.newaxis]
+    result = augmenter.augment_image(img)
+    if img.ndim == 2:
+        result = result[:, :, 0]
+    return result
 
-    Args:
-        p (float): probability of applying the transform. Default: 0.5.
 
-    Targets:
-        image, mask
-    """
-
+class IAAAffine2(DualTransform):
     def __init__(
         self,
         scale=(0.7, 1.3),
@@ -27,58 +26,64 @@ class IAAAffine2(DualIAATransform):
         always_apply=False,
         p=0.5,
     ):
-        super(IAAAffine2, self).__init__(always_apply, p)
-        self.scale = dict(x=scale, y=scale)
+        super().__init__(always_apply=always_apply, p=p)
+        self.scale = scale
         self.translate_percent = to_tuple(translate_percent, 0)
         self.translate_px = to_tuple(translate_px, 0)
         self.rotate = to_tuple(rotate)
-        self.shear = dict(x=shear, y=shear)
+        self.shear = shear
         self.order = order
         self.cval = cval
         self.mode = mode
 
-    @property
-    def processor(self):
-        return iaa.Affine(
-            self.scale,
-            self.translate_percent,
-            self.translate_px,
-            self.rotate,
-            self.shear,
-            self.order,
-            self.cval,
-            self.mode,
+    def apply(self, img, **params):
+        aug = iaa.Affine(
+            scale=dict(x=self.scale, y=self.scale),
+            translate_percent=self.translate_percent,
+            translate_px=self.translate_px,
+            rotate=self.rotate,
+            shear=dict(x=self.shear, y=self.shear),
+            order=self.order,
+            cval=self.cval,
+            mode=self.mode,
         )
+        return _apply_iaa(img, aug)
+
+    def apply_to_mask(self, mask, **params):
+        aug = iaa.Affine(
+            scale=dict(x=self.scale, y=self.scale),
+            translate_percent=self.translate_percent,
+            translate_px=self.translate_px,
+            rotate=self.rotate,
+            shear=dict(x=self.shear, y=self.shear),
+            order=0,
+            cval=self.cval,
+            mode=self.mode,
+        )
+        return _apply_iaa(mask, aug)
 
     def get_transform_init_args_names(self):
         return ("scale", "translate_percent", "translate_px", "rotate", "shear", "order", "cval", "mode")
 
 
-class IAAPerspective2(DualIAATransform):
-    """Perform a random four point perspective transform of the input.
-
-    Note: This class introduce interpolation artifacts to mask if it has values other than {0;1}
-
-    Args:
-        scale ((float, float): standard deviation of the normal distributions. These are used to sample
-            the random distances of the subimage's corners from the full image's corners. Default: (0.05, 0.1).
-        p (float): probability of applying the transform. Default: 0.5.
-
-    Targets:
-        image, mask
-    """
-
+class IAAPerspective2(DualTransform):
     def __init__(self, scale=(0.05, 0.1), keep_size=True, always_apply=False, p=0.5,
                  order=1, cval=0, mode="replicate"):
-        super(IAAPerspective2, self).__init__(always_apply, p)
+        super().__init__(always_apply=always_apply, p=p)
         self.scale = to_tuple(scale, 1.0)
         self.keep_size = keep_size
         self.cval = cval
         self.mode = mode
 
-    @property
-    def processor(self):
-        return iaa.PerspectiveTransform(self.scale, keep_size=self.keep_size, mode=self.mode, cval=self.cval)
+    def apply(self, img, **params):
+        aug = iaa.PerspectiveTransform(scale=self.scale, keep_size=self.keep_size,
+                                       mode=self.mode, cval=self.cval)
+        return _apply_iaa(img, aug)
+
+    def apply_to_mask(self, mask, **params):
+        aug = iaa.PerspectiveTransform(scale=self.scale, keep_size=self.keep_size,
+                                       mode=self.mode, cval=self.cval)
+        return _apply_iaa(mask, aug)
 
     def get_transform_init_args_names(self):
         return ("scale", "keep_size")
